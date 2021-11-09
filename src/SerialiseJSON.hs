987acf8902575b9c008cc7ef388b0a7749a9ae90
Alain Magazin <alain.magazin@outlook.com>
2021-11-09 02:25:50 +0100

    Devel (#10)
    * Fee Contract v1
    
    * Added update price endpoint
    
    * Update hie.yaml
    
    * Update README.md
    
    * Update and rename vendere.cabal to martify.cabal
    
    * Update martify.cabal
    
    * Update datum-json.hs

--- src/SerialiseJSON.hs
+++ src/SerialiseJSON.hs
@@ -23,7 +23,7 @@ import Market.Types (SaleAction(..), NFTSale(..))
 -- Constructs the JSON file for the Buy Redeemer constructor, used as input to --tx-in-redeemer-file
 testR :: IO ()
 testR = do 
-  writeData "redeemer.json" Close
+  writeData "update.json" Update
   putStrLn "Done"
 
 
